c0dff9fe5b290e8136c8815007048e51a6d0fd1b
MarvAmBass <marvambass@gmail.com>
2017-04-24 13:07:39 +0200

    added if statements if envs are set

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -40,75 +40,84 @@ if [ ! -f "$INITALIZED" ]; then
   ##
   # HTACCESS
   ##
-  for I_ACCOUNT in "$(env | grep '^HTACCESS_ACCOUNT_')"
-  do
-    ACCOUNT_NAME=$(echo "$I_ACCOUNT" | cut -d'=' -f1 | sed 's/HTACCESS_ACCOUNT_//g' | tr '[:upper:]' '[:lower:]')
-    ACCOUNT_PASSWORD=$(echo "$I_ACCOUNT" | sed 's/^[^=]*=//g')
+  if env | grep '^HTACCESS_ACCOUNT_'
+  then
+    for I_ACCOUNT in "$(env | grep '^HTACCESS_ACCOUNT_')"
+    do
+      ACCOUNT_NAME=$(echo "$I_ACCOUNT" | cut -d'=' -f1 | sed 's/HTACCESS_ACCOUNT_//g' | tr '[:upper:]' '[:lower:]')
+      ACCOUNT_PASSWORD=$(echo "$I_ACCOUNT" | sed 's/^[^=]*=//g')
 
-    echo ">> HTACCESS: adding account: $ACCOUNT_NAME"
-    echo -e "$ACCOUNT_PASSWORD\n$ACCOUNT_PASSWORD" | htpasswd $(if [ ! -e /conf/auth.htpasswd ]; then echo '-c'; fi) /conf/auth.htpasswd "$ACCOUNT_NAME"
+      echo ">> HTACCESS: adding account: $ACCOUNT_NAME"
+      echo -e "$ACCOUNT_PASSWORD\n$ACCOUNT_PASSWORD" | htpasswd $(if [ ! -e /conf/auth.htpasswd ]; then echo '-c'; fi) /conf/auth.htpasswd "$ACCOUNT_NAME"
 
-    unset $(echo "$I_ACCOUNT" | cut -d'=' -f1)
-  done
+      unset $(echo "$I_ACCOUNT" | cut -d'=' -f1)
+    done
+  fi
 
   ##
   # NGINX RAW Config ENVs
   ##
-  for I_CONF in "$(env | grep '^NGINX_RAW_CONFIG_')"
-  do
-    rm /etc/nginx/conf.d/default.conf 2> /dev/null
+  if env | grep '^NGINX_RAW_CONFIG_'
+  then
+    for I_CONF in "$(env | grep '^NGINX_RAW_CONFIG_')"
+    do
+      rm /etc/nginx/conf.d/default.conf 2> /dev/null
 
-    CONFD_CONF_NAME=$(echo "$I_CONF" | cut -d'=' -f1 | sed 's/NGINX_RAW_CONFIG_//g' | tr '[:upper:]' '[:lower:]')
-    CONFD_CONF_VALUE=$(echo "$I_CONF" | sed 's/^[^=]*=//g')
+      CONFD_CONF_NAME=$(echo "$I_CONF" | cut -d'=' -f1 | sed 's/NGINX_RAW_CONFIG_//g' | tr '[:upper:]' '[:lower:]')
+      CONFD_CONF_VALUE=$(echo "$I_CONF" | sed 's/^[^=]*=//g')
 
-    echo "$CONFD_CONF_VALUE" >> "/conf/RAW_$CONFD_CONF_NAME.conf"
-  done
+      echo "$CONFD_CONF_VALUE" >> "/conf/RAW_$CONFD_CONF_NAME.conf"
+    done
+  fi
 
   ##
   # NGINX Config ENVs
   ##
-  for I_CONF in "$(env | grep '^NGINX_CONFIG_')"
-  do
-    rm /etc/nginx/conf.d/default.conf 2> /dev/null
+  if env | grep '^NGINX_CONFIG_'
+  then
+    for I_CONF in "$(env | grep '^NGINX_CONFIG_')"
+    do
+      rm /etc/nginx/conf.d/default.conf 2> /dev/null
 
-    CONFD_CONF_NAME=$(echo "$I_CONF" | cut -d'=' -f1 | sed 's/NGINX_CONFIG_//g' | tr '[:upper:]' '[:lower:]')
-    CONFD_CONF_VALUE=$(echo "$I_CONF" | sed 's/^[^=]*=//g')
+      CONFD_CONF_NAME=$(echo "$I_CONF" | cut -d'=' -f1 | sed 's/NGINX_CONFIG_//g' | tr '[:upper:]' '[:lower:]')
+      CONFD_CONF_VALUE=$(echo "$I_CONF" | sed 's/^[^=]*=//g')
 
-    SERVER_NAMES=$(echo "$CONFD_CONF_VALUE" | sed -e 's/.*server_name \(.*\)/\1/' -e 's/;.*//g')
-    SERVER_NAME=$(echo "$SERVER_NAMES" | awk '{print $1}')
+      SERVER_NAMES=$(echo "$CONFD_CONF_VALUE" | sed -e 's/.*server_name \(.*\)/\1/' -e 's/;.*//g')
+      SERVER_NAME=$(echo "$SERVER_NAMES" | awk '{print $1}')
 
-    TLDs=$(sh -c "for i in \$(echo \"$SERVER_NAMES\" | sed 's,\.,/,g'); do basename \$i; done | sort | uniq  | tr '[:lower:]' '[:upper:]'")
-    VALID_EXTERNAL_DOMAINS=$(for I_TLD in $TLDs; do grep "^$I_TLD\$" /iana-tlds.txt 2>/dev/null >/dev/null; echo $? ; done | uniq | sort -n | head -n1)
+      TLDs=$(sh -c "for i in \$(echo \"$SERVER_NAMES\" | sed 's,\.,/,g'); do basename \$i; done | sort | uniq  | tr '[:lower:]' '[:upper:]'")
+      VALID_EXTERNAL_DOMAINS=$(for I_TLD in $TLDs; do grep "^$I_TLD\$" /iana-tlds.txt 2>/dev/null >/dev/null; echo $? ; done | uniq | sort -n | head -n1)
 
-    if [ $VALID_EXTERNAL_DOMAINS -eq 0 ]
-    then
-      echo ">> ACME: register domains: $SERVER_NAMES"
-      acme cert $SERVER_NAMES
-      cp -f /root/.config/acme/*.key /certs && cp -f /root/.config/acme/*.crt /certs
-      echo "acme cert -s 127.0.0.1:80 $SERVER_NAMES" >> /usr/local/bin/update-certs.sh
-      echo "cp -f /root/.config/acme/*.key /certs && cp -f /root/.config/acme/*.crt /certs" >> /usr/local/bin/update-certs.sh
-    else
-      echo ">> OPENSSL SelfSigned: generating self signed cert for $SERVER_NAMES"
+      if [ $VALID_EXTERNAL_DOMAINS -eq 0 ]
+      then
+        echo ">> ACME: register domains: $SERVER_NAMES"
+        acme cert $SERVER_NAMES
+        cp -f /root/.config/acme/*.key /certs && cp -f /root/.config/acme/*.crt /certs
+        echo "acme cert -s 127.0.0.1:80 $SERVER_NAMES" >> /usr/local/bin/update-certs.sh
+        echo "cp -f /root/.config/acme/*.key /certs && cp -f /root/.config/acme/*.crt /certs" >> /usr/local/bin/update-certs.sh
+      else
+        echo ">> OPENSSL SelfSigned: generating self signed cert for $SERVER_NAMES"
 
-      cp /etc/ssl/openssl.cnf /tmp
-      echo '[ subject_alt_name ]' >> /tmp/openssl.cnf
-      echo -n 'subjectAltName = ' >> /tmp/openssl.cnf
-      for I_SSL_DNS in $(echo "$SERVER_NAMES"); do echo -n "DNS:$I_SSL_DNS, "; done | sed 's/, $//g' >> /tmp/openssl.cnf
-      echo '' >> /tmp/openssl.cnf
+        cp /etc/ssl/openssl.cnf /tmp
+        echo '[ subject_alt_name ]' >> /tmp/openssl.cnf
+        echo -n 'subjectAltName = ' >> /tmp/openssl.cnf
+        for I_SSL_DNS in $(echo "$SERVER_NAMES"); do echo -n "DNS:$I_SSL_DNS, "; done | sed 's/, $//g' >> /tmp/openssl.cnf
+        echo '' >> /tmp/openssl.cnf
 
-      openssl req -x509 -newkey rsa:4096 \
-      -config /tmp/openssl.cnf \
-      -extensions subject_alt_name \
-      -days 3650 \
-      -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=$SERVER_NAME" \
-      -keyout "/certs/$SERVER_NAME.key" \
-      -out "/certs/$SERVER_NAME.crt" \
-      -nodes -sha256
-    fi
+        openssl req -x509 -newkey rsa:4096 \
+        -config /tmp/openssl.cnf \
+        -extensions subject_alt_name \
+        -days 3650 \
+        -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=$SERVER_NAME" \
+        -keyout "/certs/$SERVER_NAME.key" \
+        -out "/certs/$SERVER_NAME.crt" \
+        -nodes -sha256
+      fi
 
-    echo "server{listen 80; listen [::]:80; include /etc/nginx/snippets/letsencrypt-acme-challenge.conf; server_name $SERVER_NAMES; return 301 https://$SERVER_NAME;}" > "/conf/$CONFD_CONF_NAME.conf"
-    echo "$CONFD_CONF_VALUE" | sed 's/server_name/listen 443 ssl; ssl on; ssl_certificate \/certs\/'"$SERVER_NAME"'.crt; ssl_certificate_key \/certs\/'"$SERVER_NAME"'.key; server_name/g' >> "/conf/$CONFD_CONF_NAME.conf"
-  done
+      echo "server{listen 80; listen [::]:80; include /etc/nginx/snippets/letsencrypt-acme-challenge.conf; server_name $SERVER_NAMES; return 301 https://$SERVER_NAME;}" > "/conf/$CONFD_CONF_NAME.conf"
+      echo "$CONFD_CONF_VALUE" | sed 's/server_name/listen 443 ssl; ssl on; ssl_certificate \/certs\/'"$SERVER_NAME"'.crt; ssl_certificate_key \/certs\/'"$SERVER_NAME"'.key; server_name/g' >> "/conf/$CONFD_CONF_NAME.conf"
+    done
+  fi
 
   ##
   # Default page generation
