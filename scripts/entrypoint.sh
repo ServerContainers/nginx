43c87ece093c67ad3ada4b64ae75f1b9f4da5199
Marvin <MarvAmBass@users.noreply.github.com>
2017-12-18 12:15:10 +0100

    only redirect if not lets encrypt

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -115,7 +115,7 @@ if [ ! -f "$INITALIZED" ]; then
         -nodes -sha256
       fi
 
-      echo "server{listen 80; listen [::]:80; include /etc/nginx/snippets/letsencrypt-acme-challenge.conf; server_name $SERVER_NAMES; return 301 https://$SERVER_NAME;}" > "/conf/$CONFD_CONF_NAME.conf"
+      echo "server{listen 80; listen [::]:80; include /etc/nginx/snippets/letsencrypt-acme-challenge.conf; server_name $SERVER_NAMES; location / {return 301 https://$SERVER_NAME;}}" > "/conf/$CONFD_CONF_NAME.conf"
       echo "$CONFD_CONF_VALUE" | sed 's/server_name/listen 443 ssl; ssl on; ssl_certificate \/certs\/'"$SERVER_NAME"'.crt; ssl_certificate_key \/certs\/'"$SERVER_NAME"'.key; server_name/g' >> "/conf/$CONFD_CONF_NAME.conf"
     done
 
